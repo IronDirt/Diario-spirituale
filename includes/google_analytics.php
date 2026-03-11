94ecf722f194d7d6012f6aa61ebf0b127a2c3967
IronDirt <36108392+IronDirt@users.noreply.github.com>
2026-03-11 21:17:16 +0100

    Update Google Analytics Measurement ID in script

--- includes/google_analytics.php
+++ includes/google_analytics.php
@@ -1,12 +1,9 @@
-<?php
-// Replace G-XXXXXXXXXX with your actual Google Analytics 4 Measurement ID
-define('GA_MEASUREMENT_ID', 'G-XXXXXXXXXX');
-?>
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=<?= htmlspecialchars(GA_MEASUREMENT_ID, ENT_QUOTES, 'UTF-8') ?>"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V9H2M0SE0Q"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){window.dataLayer.push(arguments);}
+  function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', '<?= htmlspecialchars(GA_MEASUREMENT_ID, ENT_QUOTES, 'UTF-8') ?>');
+
+  gtag('config', 'G-V9H2M0SE0Q');
 </script>
